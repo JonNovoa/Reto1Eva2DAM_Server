00338e2eb142c89ead1ffd22fa6398be22ade642
INFORMATICA <INFORMATICA@DESKTOP-I8NP0CC>
2022-10-28 09:11:09 +0200

    Comentarios Cliente y Mensaje

--- src/sockets/ServerSocket.java
+++ src/sockets/ServerSocket.java
@@ -5,10 +5,37 @@
  */
 package sockets;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author somor
  */
 public class ServerSocket {
+    static final int PUERTO = 5000;
+
+    
+    public ServerSocket() {
+        try {
+            java.net.ServerSocket skServidor = new java.net.ServerSocket(PUERTO);
+            System.out.println("Escucho el puerto " + PUERTO);
+            for (int i = 0; i < 3; i++) {
+                Socket skCliente = skServidor.accept();
+                System.out.println("Sirvo al cliente "+i);
+                OutputStream aux = skCliente.getOutputStream();
+                DataOutputStream flujo = new DataOutputStream(aux);
+                flujo.writeUTF("Hola cliente "+i);
+                skCliente.close();
+            }
+            System.out.println("Demasiados clientes por hoy");
+        } catch (IOException ex) {
+            Logger.getLogger(ServerSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
